dff39ecc2f0f0248958b73a47042ca6327799931
Andrey <andrey.develop.a@gmail.com>
2024-02-10 17:09:07 +0700

    feat: Layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view />
+  <div class="bg-grey-2">
+    <router-view />
+  </div>
 </template>
 
 <script setup lang="ts"></script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,19 @@
 <template>
-  <q-layout view="lHh LpR lFf">
-    <q-drawer class="bg-purple q-py-sm q-pl-md" show-if-above> </q-drawer>
+  <q-layout view="lHh lpr lFf">
+    <q-drawer :breakpoint="1047" class="bg-purple" show-if-above>
+      <div class="logo q-px-md flex items-center">
+        <AppLogo />
+      </div>
+    </q-drawer>
+
+    <q-header class="flex items-center header">
+      <div class="humburger cursor-pointer column flex-center">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <AppLogo class="header-logo" />
+    </q-header>
 
     <q-page-container>
       <router-view />
@@ -8,4 +21,51 @@
   </q-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppLogo from 'components/icons/AppLogo.vue'
+</script>
+
+<style lang="scss" scoped>
+.humburger {
+  span {
+    display: block;
+    height: 2px;
+    width: 20px;
+    border-radius: 4px;
+    background: $purple-2;
+  }
+}
+.logo {
+  height: 72px;
+  border-bottom: 1px solid $purple-1;
+}
+.header {
+  height: 72px;
+  background: white;
+}
+.humburger {
+  width: 56px;
+  height: 56px;
+  border-right: 1px solid $purple-1;
+  row-gap: 4px;
+  display: none;
+}
+.header-logo {
+  visibility: hidden;
+}
+
+@media (max-width: $breakpoint-md-min) {
+  .header {
+    background: $purple;
+    height: 56px;
+    column-gap: 16px;
+  }
+  .humburger {
+    display: flex;
+  }
+  .header-logo {
+    visibility: visible;
+    width: 126px;
+  }
+}
+</style>
