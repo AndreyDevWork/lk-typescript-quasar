a59fe21ffa966ef7c171170694c35f5380184eac
Andrey <andrey.develop.a@gmail.com>
2024-03-07 23:22:50 +0700

    build: setup

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,71 +1,20 @@
 <template>
-  <q-layout view="lHh lpr lFf">
-    <q-drawer :breakpoint="1047" class="bg-purple" show-if-above>
-      <div class="logo q-px-md flex items-center">
-        <AppLogo />
-      </div>
-    </q-drawer>
-
-    <q-header class="flex items-center header">
-      <div class="humburger cursor-pointer column flex-center">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <AppLogo class="header-logo" />
+  <q-layout view="hHh lpR lFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>Currency Converter</q-toolbar-title>
+      </q-toolbar>
     </q-header>
 
-    <q-page-container>
+    <q-page-container class="q-mx-auto container">
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
-<script setup lang="ts">
-import AppLogo from 'components/icons/AppLogo.vue'
-</script>
-
+<script setup lang="ts"></script>
 <style lang="scss" scoped>
-.humburger {
-  span {
-    display: block;
-    height: 2px;
-    width: 20px;
-    border-radius: 4px;
-    background: $purple-2;
-  }
-}
-.logo {
-  height: 72px;
-  border-bottom: 1px solid $purple-1;
-}
-.header {
-  height: 72px;
-  background: white;
-}
-.humburger {
-  width: 56px;
-  height: 56px;
-  border-right: 1px solid $purple-1;
-  row-gap: 4px;
-  display: none;
-}
-.header-logo {
-  visibility: hidden;
-}
-
-@media (max-width: $breakpoint-md-min) {
-  .header {
-    background: $purple;
-    height: 56px;
-    column-gap: 16px;
-  }
-  .humburger {
-    display: flex;
-  }
-  .header-logo {
-    visibility: visible;
-    width: 126px;
-  }
+.container {
+  max-width: 1560px;
 }
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,20 @@
 <template>
-  <div></div>
+  <div class="q-mx-auto q-pt-md wrapper">
+    <div class="register">
+      <div class="title">Sign-up</div>
+      <q-input v-model="text" label="Standard" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
+<style lang="scss" scoped>
+.wrapper {
+  width: fit-content;
+}
+.title {
+  font-size: 30px;
+  font-weight: 500;
+  text-align: center;
+}
+</style>
