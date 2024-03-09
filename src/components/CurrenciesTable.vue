b475dcfabd66d20f7f9bdbf9a7e4ba36c2badeb9
Andrey <andrey.develop.a@gmail.com>
2024-03-09 09:13:25 +0700

    feat: CurrenciesTable.vue

---
+++ src/components/CurrenciesTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="q-pa-md">
+    <q-table
+      v-if="currencyies.length"
+      title="Currencyies"
+      :rows="props.currencyies"
+      :columns="columns"
+      row-key="name"
+      rows-per-page-options="0"
+    />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['currencyies'])
+
+const columns = [
+  {
+    label: 'Name',
+    field: 'name',
+    align: 'left',
+  },
+  {
+    label: 'CHAR CODE',
+    field: 'char_code',
+    align: 'left',
+  },
+  {
+    label: 'NOMINAL',
+    field: 'nominal',
+    align: 'left',
+  },
+  {
+    label: 'VALUE',
+    field: 'value',
+    align: 'left',
+  },
+  {
+    label: 'VUNIT RATE',
+    field: 'vunit_rate',
+    align: 'left',
+  },
+  {
+    label: 'UPDATED AT',
+    field: 'updated_at',
+    align: 'left',
+  },
+]
+</script>
--- src/pages/ConverterPage.vue
+++ src/pages/ConverterPage.vue
@@ -1,8 +1,36 @@
 <template>
-  <div class="q-mx-auto q-pt-md wrapper">OOOO</div>
+  <div class="q-mx-auto q-pt-md wrapper">
+    <CurrenciesTable :currencyies="currencyies" />
+  </div>
 </template>
 
-<script setup lang="js"></script>
+<script setup>
+import CurrenciesTable from 'components/CurrenciesTable.vue'
+import { ref } from 'vue'
+import { httpClient } from 'src/services/httpClient.js'
+import { useRouter } from 'vue-router'
+
+const currencyies = ref([])
+const router = useRouter()
+
+const headers = {
+  Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`,
+}
+const getCurrencyies = () => {
+  currencyies.value = httpClient
+    .get('/api/currency', { headers })
+    .then((response) => {
+      currencyies.value = response.data
+    })
+    .catch((error) => {
+      if (error.response && error.response.status === 401) {
+        localStorage.removeItem('ACCESS_TOKEN')
+        router.push('/')
+      }
+    })
+}
+getCurrencyies()
+</script>
 
 <style lang="scss" scoped>
 .wrapper {
